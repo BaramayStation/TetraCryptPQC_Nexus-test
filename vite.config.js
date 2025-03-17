bd69005206476d5e673e671c025a081ecf6660e5
BaramayStation <83786203+BaramayStation@users.noreply.github.com>
2025-03-17 00:08:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import topLevelAwait from 'vite-plugin-top-level-await';
 
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-    strictPort: true,
-  },
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    sourcemap: false, // Disable source maps to reduce build size
-    minify: "terser", // More memory-efficient minification
-    chunkSizeWarningLimit: 2000, // Increase threshold for chunk warnings
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) return "vendor"; // Reduce large chunks
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 8080,
+      strictPort: true
+    },
+    plugins: [react(), topLevelAwait()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "esnext",
+        supported: {
+          bigint: true
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    build: {
+      target: "esnext",
+      outDir: "dist",
+      sourcemap: true,
+      minify: "terser",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["react", "react-dom"],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1500,
     },
-  },
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
+    define: {
+      global: "globalThis",
+      "process.env": {}
+    },
+  };
 });
